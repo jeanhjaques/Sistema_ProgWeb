11af64a4d04ddaed76c6e11cfddfe448a3d974ef
Gabriel Alexandre <gabrielalexandre100398@gmail.com>
2019-09-09 11:31:47 -0400

    conectando com db

--- Usuario.php
+++ Usuario.php
@@ -1,8 +1,18 @@
 <?php
     $nome = (!empty($_GET['nome']) ? $_GET['nome'] : '');
-    $idade = (!empty($_GET['idade']) ? $_GET['idade'] : '');
+    $nsc = (!empty($_GET['nsc']) ? $_GET['nsc'] : '');
     $cpf = (!empty($_GET['cpf']) ? $_GET['cpf'] : '');
     $endereco = (!empty($_GET['endereco']) ? $_GET['endereco'] : '');
     $cep = (!empty($_GET['cep']) ? $_GET['cep'] : '');
     $num_cartao = (!empty($_GET['cartao']) ? $_GET['cartao'] : '');
+
+    // definições de host, database, usuário e senha
+    $host = "localhost";
+    $db   = "usuario";
+    $user = "root";
+    $pass = "";
+    // conecta ao banco de dados
+    $con = mysqli_connect($host, $user, $pass) or trigger_error(mysql_error(), E_USER_ERROR); 
+    // seleciona a base de dados em que vamos trabalhar
+    mysqli_select_db($con, $db); //1° parametro e o retorno do mysqli_connect e o 2° e a database
 ?>
--- index.php
+++ index.php
@@ -9,27 +9,29 @@
         <title>Teste</title>
     </head>
     <body>
-        <span><strong>Nome: </strong></span>
-        <input type="text" name="nome" value="<?php echo $nome; ?>" class="input">
-        <br>
-        <br>
-        <span><strong>Idade: </strong></span>
-        <input type="text" name="idade" value="<?php echo $idade; ?>" class="input">
-        <br>
-        <br>
-        <span><strong>CPF: </strong></span>
-        <input type="text" name="cpf" value="<?php echo $cpf; ?>" class="input">
-        <br>
-        <br>
-        <span><strong>Endereço: </strong></span>
-        <input type="text" name="endereco" value="<?php echo $endereco; ?>" class="input">
-        <br>
-        <br>
-        <span><strong>CEP: </strong></span>
-        <input type="text" name="cep" value="<?php echo $cep; ?>" class="input">
-        <br>
-        <br>
-        <span><strong>Número do Cartão: </strong></span>
-        <input type="text" name="cartao" value="<?php echo $num_cartao; ?>" class="input">
+        <form action="">
+            <span><strong>Nome: </strong></span>
+            <input type="text" name="nome" value="<?php echo $nome; ?>" class="input">
+            <br>
+            <br>
+            <span><strong>Data de Nascimento: </strong></span>
+            <input type="text" name="nsc" value="<?php echo $nsc; ?>" class="input">
+            <br>
+            <br>
+            <span><strong>CPF: </strong></span>
+            <input type="text" name="cpf" value="<?php echo $cpf; ?>" class="input">
+            <br>
+            <br>
+            <span><strong>Endereço: </strong></span>
+            <input type="text" name="endereco" value="<?php echo $endereco; ?>" class="input">
+            <br>
+            <br>
+            <span><strong>CEP: </strong></span>
+            <input type="text" name="cep" value="<?php echo $cep; ?>" class="input">
+            <br>
+            <br>
+            <span><strong>Número do Cartão: </strong></span>
+            <input type="text" name="cartao" value="<?php echo $num_cartao; ?>" class="input">
+        </form>
     </body>
 </html>
