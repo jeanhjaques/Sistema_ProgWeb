8ab654d345736dc80a5fac448920bef2781cf00a
Gabriel Alexandre <gabrielalexandre100398@gmail.com>
2019-09-25 11:56:37 -0400

    Adição de uma pagina para exibir o que foi cadastrado no BD usuario

--- PageUser.php
+++ PageUser.php
@@ -1,7 +1,5 @@
 <?php
     include "Usuario.php";
-
-    $link = mysqli_query($con, "SELECT * FROM usuario") or die($mysqli->error);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,12 +8,17 @@
         <title>Página inicial</title>
     </head>
     <body>
-        <?php while($dado = $link->fetch_array()){?>
-        <span><?php echo "Nome: ",  $dado['nome'], '<br>'; ?></span>
-        <span><?php echo "Data de nascimento: ",  $dado['nsc'], '<br>'; ?></span>
-        <span><?php echo "CPF: ", $dado['cpf'], '<br>'; ?></span>
-        <span><?php echo "Endereço: ", $dado['endereco'], '<br>'; ?></span>
-        <span><?php echo "CEP: ", $dado['cep'], '<br>'; ?></span>
+        <?php
+        $link = mysqli_query($con, "SELECT * FROM usuario") or die($mysqli->error); 
+        while($dado = $link->fetch_array()){?>
+            <span><?php echo "Nome: ",  $dado['nome'], '<br>'; ?></span>
+            <span><?php echo "Data de nascimento: ",  $dado['nsc'], '<br>'; ?></span>
+            <span><?php echo "CPF: ", $dado['cpf'], '<br>'; ?></span>
+            <span><?php echo "Endereço: ", $dado['endereco'], '<br>'; ?></span>
+            <span><?php echo "CEP: ", $dado['cep'], '<br>'; ?></span>
+            <form action="Index_Game.php">
+                <button>Xablau</button>
+            </form>
         <?php } ?>
     </body>
 </html>
